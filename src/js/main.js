cea6340e8605229b2e64f6f6b3ee0304ded669b2
kerry-li <kerryli1997@gmail.com>
2019-03-30 17:05:09 -0500

    Deleted comment block in main.js and added category map for demo.

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,7 @@
-// function start() {
-//   // 2. Initialize the JavaScript client library.
-//   gapi.client.init({
-//     'apiKey': 'AIzaSyDlVZRSyu6K-j6HYabaJblFPtmoFJZQHZA'
-//   }).then(function() {
-//     // 3. Initialize and make the API request.
-//     return gapi.client.request({
-//       'path': 'https://www.googleapis.com/youtube/v3/videos',
-//       'params': {
-//         part: 'contentDetails,snippet',
-//         chart: 'mostPopular'
-//       }
-//     })
-//   }).then(function(response) {
-//     console.log(response.result);
-//   }, function(reason) {
-//     console.log('Error: ' + reason.result.error.message);
-//   });
-// };
-// // 1. Load the JavaScript client library.
-// gapi.load('client', start);
+// TODO: Replace this with mapping obtained from YouTube.
+VideoCategory.setCategoryMap({
+  0: 'Comedy',
+  1: 'Gaming',
+  2: 'Entertainment',
+  3: 'Education'
+});
