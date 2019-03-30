a6630ce685598c12dba2a834b0d8343d1e92dc12
grahamrubinWUSTL <grahamkrubin@wustl.edu>
2019-03-30 17:55:07 -0500

    syntax fixes:

--- src/js/stacked_area_chart.js
+++ src/js/stacked_area_chart.js
@@ -1,31 +1,31 @@
-class StackedAreaChart() {
-	let points = [
-		{"Day": 1, "v1": 10, "v2": 20},
-		{"Day": 2, "v1": 15, "v2": 23},
-		{"Day": 3, "v1": 20, "v2": 30},
-		{"Day": 4, "v1": 15, "v2": 10},
-		{"Day": 5, "v1": 25, "v2": 15},
-	];
-	let videos = [];
-	const xMaxLength = 5;
-	var xScale; 
-	var yScale;
-	var svg;
-	var svgWidth;
-	var svgHeight;
-	var area;
+class StackedAreaChart {
+	// let 
+	// let videos = [];
+	// const xMaxLength = 5;
+	// var xScale; 
+	// var yScale;
+	// var svg;
+	// var svgWidth;
+	// var svgHeight;
+	// var area;
 	constructor(width, height) {
-		// this.points=points;
+		this.points = [
+			{"Day": 1, "v1": 10, "v2": 20},
+			{"Day": 2, "v1": 15, "v2": 23},
+			{"Day": 3, "v1": 20, "v2": 30},
+			{"Day": 4, "v1": 15, "v2": 10},
+			{"Day": 5, "v1": 25, "v2": 15},
+		];
 		this.svgWidth = width;
 		this.svgHeight = height;
 		this.svg = d3.select("body").append("svg").attr("width", width).attr("height", height)
-			.append("g")
+			// .append("g")
 		
 		this.xScale = d3.scaleLinear().domain([0, points.length]).range([0, svgWidth]);
 		this.yScale = d3.scaleLinear().domain([0,50]).range([0,svgHeight]);
 		updateVis();
 	}
-	function wrangle() {
+	wrangle = function() {
 		//if points > 5, remove first, push new points on to points
 		// for (let i = 0; i < newPoints.vids.length; i++ ) {
 
@@ -35,7 +35,7 @@ class StackedAreaChart() {
 		updateVis();
 	}
 
-	function updateVis() {
+	updateVis = function() {
 		// let p = [];
 		// for (let i = 0; i < 2; i++) {
 		// 	for (let j = 0; j < points.length; j++) {
@@ -46,10 +46,10 @@ class StackedAreaChart() {
 		// 	p
 		// }
 		// var area 
-		
-		this.svg.selectAll("path").data(points).enter()
-		.append("path")
-		.attr()
+
+		// this.svg.selectAll("path").data(points).enter()
+		// .append("path")
+		// .attr()
 		// .attr("d")
 	}
 
@@ -57,11 +57,9 @@ class StackedAreaChart() {
 
 	// }
 
-	function tootlTipBullshit() {
-
-	}
+	
 	//p format: [ [0,1], [1,1] .....]
-	function pointsToD(day) {
+	pointsToD = function(day) {
 		let d = "M " + String(p[0][0]) + " " + String(p[0][1]) + " ";
 		for (let i = 1; i < p.length; i++) {
 			d += " l " + String(p[i][0]) + " " + String(p[i][1]);
