163b5f1d66dc080c649bb42940e25b11eeb7c9e1
grahamrubinWUSTL <grahamkrubin@wustl.edu>
2019-04-13 16:40:45 -0500

    fixed map.js

--- docs/js/map.js
+++ docs/js/map.js
@@ -24,74 +24,14 @@ class GeoMap {
             .projection(this.projection);
         this.data;
         this.selectedCountry;
-        // this.dataProvider;
         this.drawMap();
-        // this.stackedAreaChart = stackedAreaChart;
     }
 
-<<<<<<< HEAD
-	drawMap() {
-		let map = this;
-		// let data;
-		const boxHeight = this.height/20;
-		// map.svg.append("rect")
-		// 	.attr("x", 0)
-		// 	.attr("y", this.height*(19/20))
-		// 	.attr("width", this.width/20)
-		// 	.attr("height", boxHeight)
-		// 	.attr("fill", "white")
-		d3.json("https://raw.githubusercontent.com/andybarefoot/andybarefoot-www/master/maps/mapdata/custom50.json", function(json) {
-			// map.data = json.features;
-			var countriesGroup = map.svg.append("g").attr("id", "map");
-			countriesGroup.append("rect")
-			.attr("x", 0)
-			.attr("y", 0)
-			.attr("width", map.width)
-			.attr("height", map.height);
-			let countries = countriesGroup
-			.selectAll("path")
-			.data(json.features)
-			.enter()
-			.append("path")
-			.attr("d", function(d) {
-				return map.path(d);
-			})
-			.attr("fill", function(d) {
-				return (map.availableCountries.includes(d.properties.name)) ? "red" : "white";
-			})
-			.attr("stroke-width", 1)
-			.attr("stroke", "grey")
-			.attr("id", function(d, i) {
-				return d.properties.iso_a3;
-			})
-			.attr("class", function(d) {
-				return (map.availableCountries.includes(d.properties.name)) ? "availableCountry" : "nonAvailableCountry";
-			})
-			.on("mouseover", function(d) {
-				d3.select(".countryText").remove();
-				map.svg.append("text")
-					.attr("class", "countryText")
-					.attr("x", 10+(map.width/12))
-					.attr("y", map.height*(39/40)-5)
-					.text(function() {
-						let t = d.properties.name;
-						if (!map.availableCountries.includes(d.properties.name)) t += " - N/A";
-						return t;
-					})
-					.style("text-anchor", "middle")
-=======
     drawMap() {
         let map = this;
         d3.json("data/custom50.json", function(json) {
-            // let data;
             const boxHeight = map.height / 20;
-            // map.svg.append("rect")
-            // 	.attr("x", 0)
-            // 	.attr("y", this.height*(19/20))
-            // 	.attr("width", this.width/20)
-            // 	.attr("height", boxHeight)
-            // 	.attr("fill", "white")
-            // map.data = json.features;
+            console.log(json.features);
             var countriesGroup = map.svg.append("g")
                 .attr("id", "map");
             countriesGroup.append("rect")
@@ -116,120 +56,48 @@ class GeoMap {
                     return d.properties.iso_a3;
                 })
                 .attr("class", function(d) {
+                    // console.log(d)
                     return (map.availableCountries.includes(d.properties.name)) ? "availableCountry" : "nonAvailableCountry";
                 })
-
                 .on("mouseover", function(d) {
-                    d3.select(".countryText")
-                        .remove();
+                    d3.select(".countryText").remove();
                     map.svg.append("text")
                         .attr("class", "countryText")
                         .attr("x", 10 + (map.width / 12))
                         .attr("y", map.height * (39 / 40) - 5)
                         .text(function() {
                             let t = d.properties.name;
-                            if (!map.availableCountries.includes(d.properties.name)) {
-                                t += " - N/A"
-                            }
+                            if (!map.availableCountries.includes(d.properties.name)) t += " - N/A";
                             return t;
                         })
-                        .style("text-anchor", "middle")
->>>>>>> ecf578cf7c076fc0fc2625f677f4da5fa86f8534
-
+                        .style("text-anchor", "middle");
                 })
-                // .on("mousemove", function(d) {
-
-<<<<<<< HEAD
-			// 	// let coordinates = d3.mouse(this);
-			// 	// map.svg.append("text")
-			// 	// 	.attr("x", coordinates[0])
-			// 	// 	.attr("y", coordinates[1])
-			// 	// 	// .attr("class", function(d) {
-			// 	// 	// 	return (map.availableCountries.includes(d.properties.name)) ? "availableCountry" : "nonAvailableCountry";
-			// 	// 	// })
-			// 	// 	.text(d.properties.name)
-			// 	// 	.style("text-anchor", "middle");
-			// })
-			.on("mouseout", function() {
-				d3.select(".countryText").remove();
-				console.log("LLLLLLLLLLL FAT L")
-				console.log(map.selectedCountry)
-				if (map.selectedCountry !== "") {
-					map.svg.append("text")
-					.attr("class", "countryText")
-					.attr("x", 10+(map.width/12))
-					.attr("y", map.height*(39/40)-5)
-					.text(map.selectedCountry)
-					.style("text-anchor", "middle")
-				}
-
-			})
-			.on("click", function(d, i) {
-
-				d3.select("#areaChartSVG").remove();
-				// map.stackedAreaChart.beginDataCollection();
-				map.selectedCountry = d.properties.name;
-				console.log(d.properties.name);
-				var dataProvider = FakeDataProvider.withNumVideos(1000, 50, 4);
-				var chart = new StackedAreaChart(dataProvider);
-				console.log(chart);
-			});
-			map.svg.append("rect")
-			.attr("x", 10)
-			.attr("y", map.height*(19/20)-10)
-			.attr("width", map.width/6)
-			.attr("height", boxHeight)
-			.attr("fill", "white")
+			    .on("mouseout", function() {
+				    d3.select(".countryText").remove();
+				    if (map.selectedCountry !== "") {
+					   map.svg.append("text")
+					   .attr("class", "countryText")
+					   .attr("x", 10+(map.width/12))
+					   .attr("y", map.height*(39/40)-5)
+					   .text(map.selectedCountry)
+					   .style("text-anchor", "middle")
+				    }
+			    })
+			    .on("click", function(d, i) {
+				    d3.select("#areaChartSVG").remove();
+				    map.selectedCountry = d.properties.name;
+				    console.log(d.properties.name);
+				    var dataProvider = FakeDataProvider.withNumVideos(1000, 50, 4);
+				    var chart = new StackedAreaChart(dataProvider);
+				    console.log(chart);
+			    });
+			    map.svg.append("rect")
+			    .attr("x", 10)
+			    .attr("y", map.height*(19/20)-10)
+			    .attr("width", map.width/6)
+			    .attr("height", boxHeight)
+			    .attr("fill", "white")
 		});
-		// this.data = map.data;
-		// console.log(this.data);
 	}
-=======
-                // 	// let coordinates = d3.mouse(this);
-                // 	// map.svg.append("text")
-                // 	// 	.attr("x", coordinates[0])
-                // 	// 	.attr("y", coordinates[1])
-                // 	// 	// .attr("class", function(d) {
-                // 	// 	// 	return (map.availableCountries.includes(d.properties.name)) ? "availableCountry" : "nonAvailableCountry";
-                // 	// 	// })
-                // 	// 	.text(d.properties.name)
-                // 	// 	.style("text-anchor", "middle");
-                // })
-                .on("mouseoff", function() {
-
-                })
-                .on("click", function(d, i) {
-                    d3.select("#areaChartSVG")
-                        .remove();
-                    // map.stackedAreaChart.beginDataCollection();
-                    map.selectedCountry = d.properties.name;
-                    console.log(d.properties.name);
-                    var dataProvider = FakeDataProvider.withNumVideos(1000, 50, 4);
-                    var chart = new StackedAreaChart(dataProvider);
-                    console.log(chart);
-                });
-            map.svg.append("rect")
-                .attr("x", 10)
-                .attr("y", map.height * (19 / 20) - 10)
-                .attr("width", map.width / 6)
-                .attr("height", boxHeight)
-                .attr("fill", "white")
-        });
-        // this.data = map.data;
-        // console.log(this.data);
-    }
->>>>>>> ecf578cf7c076fc0fc2625f677f4da5fa86f8534
-
 
 }
-
-
-// const width = 1200;
-// const height = 500;
-// var projection = d3.geoEquirectangular()
-// 	.center([0,15])
-// 	.scale([width/(2*Math.PI)])
-// 	.translate([width/2,height/2]);
-// var path = d3.geoPath()
-// 	.projection(projection);
-// const availableCountries = ["United States", "Canada", "Mexico", "United Kingdom", "Russia", "Brazil", "Australia", "India", "Turkey", "Japan"];
